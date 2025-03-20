b7739de47fdd17c956262ad200d28d31dcf97cdc
xVennce <145119672+xVennce@users.noreply.github.com>
2025-03-20 02:19:34 +0000

    Edited GameManager, FPSUiManager and PauseManager to centralise pause functions

--- MasterGameFile/DIG5116-Co-lab/Assets/Scripts/GameManager.cs
+++ MasterGameFile/DIG5116-Co-lab/Assets/Scripts/GameManager.cs
@@ -5,4 +5,27 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public bool isGamePaused = false;
+
+
+    //
+    // These functions are just used to normalise pausing in both PauseManager and FPSUiManager
+    //
+
+    /// <summary>
+    /// This function pauses the game
+    /// </summary>
+    public void PauseGame()
+    {
+        isGamePaused = true;
+        Time.timeScale = 0;
+    }
+
+    /// <summary>
+    /// This function unpauses the game
+    /// </summary>
+    public void UnPauseGame()
+    {
+        isGamePaused = false;
+        Time.timeScale = 1;
+    }
 }
