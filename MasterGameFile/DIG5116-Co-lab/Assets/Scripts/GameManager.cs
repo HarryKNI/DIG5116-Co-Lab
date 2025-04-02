d4205215345c164abc8b06ffead1a60658515345
xVennce <145119672+xVennce@users.noreply.github.com>
2025-04-02 02:35:58 +0100

    Reworked how tasks worked so the tasks now have better interaction and visual feedback

--- MasterGameFile/DIG5116-Co-lab/Assets/Scripts/GameManager.cs
+++ MasterGameFile/DIG5116-Co-lab/Assets/Scripts/GameManager.cs
@@ -6,6 +6,25 @@ public class GameManager : MonoBehaviour
 {
     public bool isGamePaused = false;
 
+    [Header("Task Completion Booleans")]
+    public bool isTask1Completed = false;
+    public bool isTask2Completed = false;
+    public bool isTask3Completed = false;
+
+    [Header("Task Incrementers")]
+    public int Task1Incrementer = 0;
+    public int Task2Incrementer = 0;
+    public int Task3Incrementer = 0;
+
+    [Header("Task Limit Ints")]
+    [SerializeField] int Task1Limit = 0;
+    [SerializeField] int Task2Limit = 0;
+    [SerializeField] int Task3Limit = 0;
+
+    private void Update()
+    {
+        CheckTaskCompletion();
+    }
 
     //
     // These functions are just used to normalise pausing in both PauseManager and FPSUiManager
@@ -27,5 +46,62 @@ public class GameManager : MonoBehaviour
     {
         isGamePaused = false;
         Time.timeScale = 1;
+    }
+
+
+    /// <summary>
+    /// This function increments task 1 number
+    /// </summary>
+    public void IncrementTask1()
+    {
+        ++Task1Incrementer;
+        if (Task1Incrementer >= Task1Limit)
+        {
+            Task1Incrementer = Task1Limit;
+        }
+
+    }
+
+    /// <summary>
+    /// This function increments task 2 number
+    /// </summary>
+    public void IncrementTask2()
+    {
+        ++Task2Incrementer;
+        if (Task2Incrementer >= Task2Limit)
+        {
+            Task2Incrementer = Task2Limit;
+        }
+    }
+
+    /// <summary>
+    /// This function increments task 3 number
+    /// </summary>
+    public void IncrementTask3()
+    {
+        ++Task3Incrementer;
+        if (Task3Incrementer >= Task3Limit)
+        {
+            Task3Incrementer = Task3Limit;
+        }
+    }
+
+    /// <summary>
+    /// This function checks if the task is completed
+    /// </summary>
+    private void CheckTaskCompletion()
+    {
+        if (Task1Incrementer >= Task1Limit)
+        {
+            isTask1Completed = true;
+        }
+        if (Task2Incrementer >= Task2Limit)
+        {
+            isTask2Completed = true;
+        }
+        if (Task3Incrementer >= Task3Limit)
+        {
+            isTask3Completed = true;
+        }
     }
 }
